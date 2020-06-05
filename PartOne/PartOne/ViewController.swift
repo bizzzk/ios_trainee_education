a103a0ecf01521c0ea9dc41d02c354c460acaa79
bizzzk <bizkitonoviy@gmail.com>
2020-06-05 13:03:06 +0300

    Added icons, app name, splash screen

--- PartOne/PartOne/ViewController.swift
+++ PartOne/PartOne/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
 }
+
 
